14f67fb5b85b3113a379dc5ee15b62c167562138
chanan-duy <chanan.duy@proton.me>
2025-10-28 21:08:30 +0500

    `TwentyOneGame.Test` init

---
+++ TwentyOneGame.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace TwentyOneGame.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
