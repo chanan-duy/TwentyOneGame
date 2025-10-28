68dbce38812b4a7b1134a241c7f8a4a4283ebe2d
chanan-duy <chanan.duy@proton.me>
2025-10-28 23:43:54 +0500

    `Deck` init with `DeckTest`

---
+++ TwentyOneGame.Test/DeckTest.cs
@@ -0,0 +1,21 @@
+﻿namespace TwentyOneGame.Test;
+
+public class DeckTest
+{
+    [Test]
+    public void ConstructorTest()
+    {
+        var deck = new Deck();
+
+        Assert.That(deck, Is.Not.EqualTo(null));
+    }
+
+    [Test]
+    public void FillTest()
+    {
+        var deck = new Deck();
+        deck.FillCardsRandomized();
+
+        Assert.That(deck.Cards, Is.All.Not.EqualTo(null));
+    }
+}
---
+++ TwentyOneGame/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace TwentyOneGame;
+
+public class Deck
+{
+    public const int DeckSize = 4 * 9;
+    public readonly Card[] Cards;
+
+    public Deck()
+    {
+        Cards = new Card[DeckSize];
+    }
+
+    public void FillCardsRandomized()
+    {
+        var index = 0;
+        foreach (var suit in Enum.GetValues<Suit>())
+        {
+            foreach (var rank in Enum.GetValues<Rank>())
+            {
+                Cards[index++] = new Card(suit, rank);
+            }
+        }
+
+        RandomNumberGenerator.Shuffle(Cards.AsSpan());
+    }
+}
