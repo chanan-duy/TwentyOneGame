861832b6abea456089387fa07deac11ac53c7e34
chanan-duy <chanan.duy@proton.me>
2025-10-28 22:44:52 +0500

    `Card` init with `CardTest`

---
+++ TwentyOneGame.Test/CardTest.cs
@@ -0,0 +1,20 @@
+﻿namespace TwentyOneGame.Test;
+
+public class CardTest
+{
+    [Test]
+    public void ConstructorTest()
+    {
+        const Suit suit = Suit.Diamonds;
+        const Rank rank = Rank.Queen;
+
+        var card = new Card(suit, rank);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(card, Is.Not.EqualTo(null));
+            Assert.That(card.Suit, Is.EqualTo(suit));
+            Assert.That(card.Rank, Is.EqualTo(rank));
+        });
+    }
+}
---
+++ TwentyOneGame/Card.cs
@@ -0,0 +1,34 @@
+﻿namespace TwentyOneGame;
+
+public class Card
+{
+    public readonly Suit Suit;
+    public readonly Rank Rank;
+
+    public Card(Suit suit, Rank rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+}
+
+public enum Suit
+{
+    Hearts = 0,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+public enum Rank
+{
+    Ace = 0,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
