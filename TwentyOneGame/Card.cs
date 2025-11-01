6286373ffd3d8a1d1e355b2d9332d214da48fb93
chanan-duy <chanan.duy@proton.me>
2025-11-01 15:58:05 +0500

    `Card` add `Value`

--- TwentyOneGame/Card.cs
+++ TwentyOneGame/Card.cs
@@ -4,11 +4,30 @@ public class Card
 {
     public readonly Suit Suit;
     public readonly Rank Rank;
+    public readonly int Value;
 
     public Card(Suit suit, Rank rank)
     {
         Suit = suit;
         Rank = rank;
+        Value = RankToValue(Rank);
+    }
+
+    private static int RankToValue(Rank rank)
+    {
+        return rank switch
+        {
+            Rank.Six => 6,
+            Rank.Seven => 7,
+            Rank.Eight => 8,
+            Rank.Nine => 9,
+            Rank.Ten => 10,
+            Rank.Jack => 2,
+            Rank.Queen => 3,
+            Rank.King => 4,
+            Rank.Ace => 11,
+            _ => throw new ArgumentOutOfRangeException(nameof(rank), rank, null),
+        };
     }
 }
 
